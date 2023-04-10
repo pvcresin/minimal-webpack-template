377a6d34f746bade6e564f21a1d7bade214174a0
pvcresin <pvcresin0730@gmail.com>
2023-04-10 17:06:12 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "Hello";
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+/** @type import('webpack').Configuration */
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+};
